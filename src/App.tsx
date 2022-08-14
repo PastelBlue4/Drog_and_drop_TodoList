29202ec9bbff2890fe163e202bda47c39dd9ec03
Romuru <pastelblue0721@gmail.com>
2022-08-14 23:13:59 +0900

    grobal styled(reset)  add left side hambuger menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const Li = styled.li`
+  display: flex;
+  font-size: 32px;
+`;
+
+const Span = styled.span`
+  width: 50px;
+  height: 50px;
+  display: flex;
+`;
 
 function App() {
   const onDragEnd = () => {};
@@ -6,13 +18,54 @@ function App() {
     <DragDropContext onDragEnd={onDragEnd}>
       <div>
         <Droppable droppableId="one">
-          {() => (
-            <ul>
+          {(provided) => (
+            <ul ref={provided.innerRef} {...provided.droppableProps}>
               <Draggable draggableId="first" index={0}>
-                {() => <li>one</li>}
+                {(provided) => (
+                  <Li ref={provided.innerRef} {...provided.draggableProps}>
+                    {" "}
+                    <Span {...provided.dragHandleProps}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M4 6h16M4 12h16M4 18h16"
+                        />
+                      </svg>
+                    </Span>
+                    one
+                  </Li>
+                )}
               </Draggable>
               <Draggable draggableId="second" index={1}>
-                {() => <li>two</li>}
+                {(provided) => (
+                  <Li ref={provided.innerRef} {...provided.draggableProps}>
+                    <Span {...provided.dragHandleProps}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M4 6h16M4 12h16M4 18h16"
+                        />
+                      </svg>
+                    </Span>
+                    two
+                  </Li>
+                )}
               </Draggable>
             </ul>
           )}
